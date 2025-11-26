a0a2fd9483f9cc61117c439a37970641eaeb2eb4
uday.singh <uday.singh@servicenow.com>
2025-11-26 13:20:32 +0530

    all apis done for employees

--- src/main/java/com/ups/cohort/controllers/EmployeeController.java
+++ src/main/java/com/ups/cohort/controllers/EmployeeController.java
@@ -1,7 +1,14 @@
 package com.ups.cohort.controllers;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +30,57 @@ public class EmployeeController {
 	public EmployeeDto createEmployee(@RequestBody EmployeeDto employeeDto) {
 		return employeeService.createEmployee(employeeDto);
 	}
+
+	@GetMapping("/{employeeId}")
+	public EmployeeDto getEmployee(@PathVariable("employeeId") Long employeeId) {
+		return employeeService.getEmployeeById(employeeId);
+	}
+
+	@GetMapping()
+	public List<EmployeeDto> getAllEmployees(){
+		return employeeService.getAllEmployees();
+	}
+
+	@PutMapping("/{employeeId}")
+	public EmployeeDto updateEmployee(@PathVariable Long employeeId,
+	                                  @RequestBody EmployeeDto employeeDto){
+		return employeeService.updateEmployee(employeeId , employeeDto);
+	}
+
+	@PatchMapping("/{employeeId}")
+	public EmployeeDto updateEmployeePartially(@PathVariable Long employeeId,
+	                                           @RequestBody Map<String , Object> updates){
+		return employeeService.updateEmployeePartially(employeeId , updates);
+	}
+
+
+
+	@DeleteMapping("/{employeeId}")
+	public String deleteEmployee(@PathVariable Long employeeId){
+		employeeService.deleteEmployee(employeeId);
+		return "employee Deleted with id " + employeeId;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	@GetMapping("/health")
 	public String healthCheck() {
--- src/main/java/com/ups/cohort/services/EmployeeService.java
+++ src/main/java/com/ups/cohort/services/EmployeeService.java
@@ -1,5 +1,8 @@
 package com.ups.cohort.services;
 
+import java.util.List;
+import java.util.Map;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +16,7 @@ public class EmployeeService {
 	private final EmployeeRepository employeeRepository;
 	private final ModelMapper modelMapper;
 
-	public EmployeeService(EmployeeRepository employeeRepository , ModelMapper modelMapper) {
+	public EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper) {
 		this.employeeRepository = employeeRepository;
 		this.modelMapper = modelMapper;
 	}
@@ -23,4 +26,61 @@ public class EmployeeService {
 		employeeEntity = employeeRepository.save(employeeEntity);
 		return modelMapper.map(employeeEntity, EmployeeDto.class);
 	}
+
+	public EmployeeDto getEmployeeById(Long employeeId) {
+		EmployeeEntity employeeEntity = employeeRepository.findById(employeeId).orElse(null);
+		return modelMapper.map(employeeEntity, EmployeeDto.class);
+	}
+
+	public List<EmployeeDto> getAllEmployees() {
+		List<EmployeeEntity> employeeEntities = employeeRepository.findAll();
+		return employeeEntities
+				.stream()
+				.map(employeeEntity -> modelMapper.map(employeeEntity, EmployeeDto.class))
+				.toList();
+	}
+
+	public EmployeeDto updateEmployee(Long employeeId, EmployeeDto employeeDto) {
+		EmployeeEntity employeeEntity = employeeRepository.findById(employeeId).orElse(null);
+
+
+		employeeEntity.setEmail(employeeDto.getEmail());
+		employeeEntity.setDepartment(employeeDto.getDepartment());
+		employeeEntity.setName(employeeDto.getName());
+		employeeEntity.setSalary(employeeDto.getSalary());
+
+		return modelMapper.map(employeeRepository.save(employeeEntity) , EmployeeDto.class);
+
+	}
+
+	public EmployeeDto updateEmployeePartially(Long employeeId, Map<String, Object> updates) {
+		EmployeeEntity employeeEntity = employeeRepository.findById(employeeId).orElse(null);
+
+		updates.forEach((key , value) -> {
+			switch (key) {
+				case "name":
+					employeeEntity.setName((String) value);
+					break;
+				case "email":
+					employeeEntity.setEmail((String) value);
+					break;
+
+				case "department":
+					employeeEntity.setDepartment((String) value);
+					break;
+				case "salary":
+					employeeEntity.setSalary((double) value);
+					break;
+
+			}
+		});
+
+		return modelMapper.map(employeeRepository.save(employeeEntity) , EmployeeDto.class);
+
+	}
+
+	public void deleteEmployee(Long employeeId) {
+		employeeRepository.deleteById(employeeId);
+	}
+
 }
